4932953d9049c3c16c810583c5a23e1b52409c06
MartinBINARD <martin.binard.dev@gmail.com>
2024-03-05 14:25:27 +0100

    Add Delete recipe admin + refactor admin recipe form

--- src/pages/Admin/pages/AdminRecipes/pages/AdminRecipesForm/AdminRecipesForm.jsx
+++ src/pages/Admin/pages/AdminRecipes/pages/AdminRecipesForm/AdminRecipesForm.jsx
@@ -1,27 +1,24 @@
-import styles from './AdminRecipeForm.module.scss';
-import * as yup from 'yup';
-import { useForm } from 'react-hook-form';
-import { yupResolver } from '@hookform/resolvers/yup';
-import { useContext } from 'react';
-import { ApiContext } from '../../../../../../contexts/ApiContext';
+import styles from "./AdminRecipesForm.module.scss";
+import * as yup from "yup";
+import { useForm } from "react-hook-form";
+import { yupResolver } from "@hookform/resolvers/yup";
+import { createRecipe } from "../../../../../../apis";
 
-function AdminRecipeForm() {
-  const BASE_URL = useContext(ApiContext);
-
+function AdminRecipesForm() {
   const defaultValues = {
-    title: '',
-    image: '',
+    title: "",
+    image: "",
   };
 
   const recipeSchema = yup.object({
     title: yup
       .string()
-      .required('Le titre de la recette doit être renseigné')
-      .min(10, 'Le titre doit être explicite')
-      .max(30, 'Le titre doit être succinct'),
+      .required("Le titre de la recette doit être renseigné")
+      .min(10, "Le titre doit être explicite")
+      .max(30, "Le titre doit être succinct"),
     image: yup
       .string()
-      .required('Il faut renseigner une image')
+      .required("Il faut renseigner une image")
       .url("L'image doit être un lien valide"),
   });
 
@@ -40,23 +37,10 @@ function AdminRecipeForm() {
   async function submit(values) {
     try {
       clearErrors();
-      const response = await fetch(BASE_URL, {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify(values),
-      });
-      if (response.ok) {
-        reset(defaultValues);
-      } else {
-        setError('generic', {
-          type: 'generic',
-          message: 'Il y a eu une erreur',
-        });
-      }
+      await createRecipe(values);
+      reset(defaultValues);
     } catch (e) {
-      setError('generic', { type: 'generic', message: 'Il y a eu une erreur' });
+      setError("generic", { type: "generic", message: "Il y a eu une erreur" });
     }
   }
 
@@ -68,12 +52,12 @@ function AdminRecipeForm() {
       <h2 className="mb-20">Ajouter une recette</h2>
       <div className="d-flex flex-column mb-20">
         <label>Titre de la recette</label>
-        <input {...register('title')} type="text" />
+        <input {...register("title")} type="text" />
         {errors.title && <p className="form-error">{errors.title.message}</p>}
       </div>
       <div className="d-flex flex-column mb-20">
         <label>Image pour la recette</label>
-        <input {...register('image')} type="text" />
+        <input {...register("image")} type="text" />
         {errors.image && <p className="form-error">{errors.image.message}</p>}
       </div>
       {errors.generic && <p className="form-error">{errors.generic.message}</p>}
@@ -86,4 +70,4 @@ function AdminRecipeForm() {
   );
 }
 
-export default AdminRecipeForm;
+export default AdminRecipesForm;
